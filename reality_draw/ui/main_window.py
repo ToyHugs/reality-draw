2fa6fcc6f19d2aaa9c78b907b3e9d1f6da1405ce
ToyHugs <pierre.guyot@toyhugs.fr>
2025-05-13 22:10:20 +0200

    fix(ui): filedialog filetypes

--- reality_draw/ui/main_window.py
+++ reality_draw/ui/main_window.py
@@ -1,6 +1,7 @@
 import tkinter as tk
 from tkinter import filedialog  # Import pour la boîte de dialogue de fichier
 
+
 class MainWindow(object):
     def __init__(self, root: tk.Tk):
         self.root = root
@@ -11,15 +12,18 @@ class MainWindow(object):
         self.main_frame.pack(fill=tk.BOTH, expand=True)
 
         # Create a label for the title
-        self.title_label = tk.Label(self.main_frame, text="Reality Draw", font=("Helvetica", 24))
+        self.title_label = tk.Label(
+            self.main_frame, text="Reality Draw", font=("Helvetica", 24))
         self.title_label.pack(pady=20)
 
         # Create a button to start drawing
-        self.draw_button = tk.Button(self.main_frame, text="Start Drawing", command=self.start_drawing)
+        self.draw_button = tk.Button(
+            self.main_frame, text="Start Drawing", command=self.start_drawing)
         self.draw_button.pack(pady=10)
 
         # Create a button to upload an image
-        self.upload_button = tk.Button(self.main_frame, text="Upload Image", command=self.upload_image)
+        self.upload_button = tk.Button(
+            self.main_frame, text="Upload Image", command=self.upload_image)
         self.upload_button.pack(pady=10)
 
     def start_drawing(self):
@@ -30,7 +34,7 @@ class MainWindow(object):
         # Ouvrir une boîte de dialogue pour sélectionner un fichier
         file_path = filedialog.askopenfilename(
             title="Select an Image",
-            filetypes=[("Image Files", "*.png;*.jpg;*.jpeg")]
+            filetypes=[("Image files", "*.png *.jpg *.jpeg")]
         )
         if file_path:
             print(f"Image selected: {file_path}")
